2c9c9db6aa4d33230e23acf44f53569e0e5f3709
parkjye <parkjye44@gmail.com>
2020-07-05 05:10:37 +0900

    [추가] PlayListDao

--- Web03_PlayList/src/test/playlist/dao/PlaylistDao.java
+++ Web03_PlayList/src/test/playlist/dao/PlaylistDao.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import test.playlist.dto.PlayListDto;
+import test.playlist.dto.PlaylistDto;
 import test.util.DbcpBean;
 
 public class PlaylistDao {
@@ -20,10 +20,10 @@ public class PlaylistDao {
 		return dao;
 	}
 	
-	//return PlayList 메소드
-	public List<PlayListDto> getList(){
+	//[return] PlayList 전체
+	public List<PlaylistDto> getList(){
 		
-		List<PlayListDto> list = new ArrayList<>(); 
+		List<PlaylistDto> list = new ArrayList<>(); 
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -31,7 +31,7 @@ public class PlaylistDao {
 		try { 
 			conn = new DbcpBean().getConn();
 			
-			String sql = "select num, check, title, singer, TO_CHAR(time, 'MI:SS') as time"
+			String sql = "select num, checked, title, singer, TO_CHAR(time, 'MI:SS') as time"
 					+ " from playlist"
 					+ " order by num asc";
 			
@@ -40,9 +40,9 @@ public class PlaylistDao {
 			
 			//반복문 돌면서 결과 값 추출하기 
 			while (rs.next()) {
-				PlayListDto dto = new PlayListDto();
+				PlaylistDto dto = new PlaylistDto();
 				dto.setNum(rs.getInt("num"));
-				dto.setCheck(rs.getString("check"));
+				dto.setChecked(rs.getString("checked"));
 				dto.setTitle(rs.getString("title"));
 				dto.setSinger(rs.getString("singer"));
 				dto.setTime(rs.getString("time"));
@@ -65,7 +65,48 @@ public class PlaylistDao {
 		return list;
 	}//getList()
 	
-	//list 삭제 (+check를 활용해서 다중 삭제)
+	//[return] list 한 개
+	public PlaylistDto getSingleList(int num) {
+		PlaylistDto dto = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = new DbcpBean().getConn();
+			
+			String sql="select num, checked, title, singer, time"
+					+ " from playlist"
+					+ " where num=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			
+			//select된 결과가 있으면 Dto에
+			if(rs.next()) {
+				dto = new PlaylistDto();
+				dto.setNum(num);
+				dto.setChecked(rs.getString("checked"));
+				dto.setTitle(rs.getString("title"));
+				dto.setSinger(rs.getString("singer"));
+				dto.setTime(rs.getString("time"));
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return dto;
+	}
+	
+	//[return] list 삭제 (+check를 활용해서 다중 삭제)
 	public boolean delete(int num) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -100,8 +141,8 @@ public class PlaylistDao {
 		}
 	}
 	
-	//추가 (+check는 default값을 false // time은 sysdate로 처리)
-	public boolean insert(PlayListDto dto) {
+	//[return] 노래 추가
+	public boolean insert(PlaylistDto dto) {
 		//필요한 객체의 참조값을 담을 지역변수 만들기 
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -112,7 +153,7 @@ public class PlaylistDao {
 			conn = new DbcpBean().getConn();
 			//실행할 sql 문 준비하기
 			String sql = "insert into playlist"
-					+ "(num, check, title, singer, time)"
+					+ "(num, checked, title, singer, time)"
 					+ "values(playlist_seq.nextval, 'false', ?, ?, sysdate)";
 			pstmt = conn.prepareStatement(sql);
 			
@@ -142,8 +183,8 @@ public class PlaylistDao {
 		}
 	}
 	
-	//수정
-	public boolean update(PlayListDto dto) {
+	//[return] 수정
+	public boolean update(PlaylistDto dto) {
 		//필요한 객체의 참조값을 담을 지역변수 만들기 
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -160,6 +201,7 @@ public class PlaylistDao {
 			//sql 문에 ? 에 바인딩할 값이 있으면 바인딩하고
 			pstmt.setString(1, dto.getTitle());
 			pstmt.setString(2, dto.getSinger());
+			pstmt.setInt(3, dto.getNum());
 
 			//sql문 수행하고 update or insert or delete된 row의 갯수 리턴받기
 			flag = pstmt.executeUpdate();
@@ -168,10 +210,8 @@ public class PlaylistDao {
 			e.printStackTrace();
 		} finally {
 			try {
-				if (pstmt != null)
-					pstmt.close();
-				if (conn != null)
-					conn.close();
+				if (pstmt != null)pstmt.close();
+				if (conn != null)conn.close();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -182,4 +222,5 @@ public class PlaylistDao {
 			return false;
 		}
 	}
+ 
 }//PlaylistDao
